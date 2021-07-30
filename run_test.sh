bab0d380c0b770483cb65da83b19cd69f5fc1de2
denise.worthen <Denise.Worthen@noaa.gov>
2021-07-30 21:20:24 +0000

    remove un-needed quotes

--- run_test.sh
+++ run_test.sh
@@ -13,35 +13,35 @@ function edit_namelist {
 }
 
 export RESNAME=$1
-export DEBUG='.F.'
-export MASKEDIT='.F.'
-export OUTDIR_PATH='/scratch2/NCEPDEV/climate/Denise.Worthen/grids-ufs/'
+export DEBUG=.F.
+export MASKEDIT=.F.
+export OUTDIR_PATH=/scratch2/NCEPDEV/climate/Denise.Worthen/grids-ufs/
 
-if [ $RESNAME = '400' ]; then
-  export FIXDIR_PATH='/scratch2/NCEPDEV/climate/Denise.Worthen/soca/test/Data/72x35x25/INPUT/'
+if [ $RESNAME = 400 ]; then
+  export FIXDIR_PATH=/scratch2/NCEPDEV/climate/Denise.Worthen/soca/test/Data/72x35x25/INPUT/
 else
-  export FIXDIR_PATH='/scratch2/NCEPDEV/climate/climpara/S2S/FIX/fix_UFSp4/fix_mom6/${RESNAME}/'
+  export FIXDIR_PATH=/scratch2/NCEPDEV/climate/climpara/S2S/FIX/fix_UFSp4/fix_mom6/${RESNAME}/
 fi
 
-if [ $RESNAME = "400" ]; then
-  export NI='72'
-  export NJ='35'
+if [ $RESNAME = 400 ]; then
+  export NI=72
+  export NJ=35
 fi
 
-if [ $RESNAME = "100" ]; then
-  export NI='360'
-  export NJ='320'
-  export MASKEDIT='.T.'
+if [ $RESNAME = 100 ]; then
+  export NI=360
+  export NJ=320
+  export MASKEDIT=.T.
 fi
 
-if [ $RESNAME = "050" ]; then
-  export NI='720'
-  export NJ='576'
+if [ $RESNAME = 050 ]; then
+  export NI=720
+  export NJ=576
 fi
 
-if [ $RESNAME = "025" ]; then
-  export NI='1440'
-  export NJ='1080'
+if [ $RESNAME = 025 ]; then
+  export NI=1440
+  export NJ=1080
 fi
 
 edit_namelist < grid.nml.IN > grid.nml
