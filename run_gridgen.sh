5764a54246e9a8c27d0cc7d9214c64dc980d94c7
Denise Worthen <denise.worthen@noaa.gov>
2024-02-19 09:06:15 -0500

    update run script

--- run_gridgen.sh
+++ run_gridgen.sh
@@ -22,19 +22,20 @@ export RESNAME=$1
 export MOSAICRES=$2
 export DEBUG=.false.
 export MASKEDIT=.false.
-export DO_POSTWGTS=.false.
+export DO_POSTWGTS=.true.
 #export OUTDIR_PATH=/scratch2/NCEPDEV/climate/Denise.Worthen/grids-20210727/
 #export OUTDIR_PATH=/scratch2/NCEPDEV/climate/Denise.Worthen/grids-esmf-20210822/
 #export OUTDIR_PATH=/scratch2/NCEPDEV/climate/Denise.Worthen/grids-esmf-20211107
-export OUTDIR_PATH=/scratch1/NCEPDEV/climate/Denise.Worthen/grids-test-202210
+#export OUTDIR_PATH=/scratch1/NCEPDEV/climate/Denise.Worthen/grids-test-202210
+export OUTDIR_PATH=/scratch1/NCEPDEV/climate/Denise.Worthen/grids-20240218
 export MOSAICDIR_PATH=/scratch1/NCEPDEV/global/glopara/fix/orog/20220805
+export APRUN='srun -A nems --nodes=1 -t 00:30:00'
 
-if [ $RESNAME = 400 ]; then
-  export FIXDIR_PATH=/scratch2/NCEPDEV/climate/Denise.Worthen/soca/test/Data/72x35x25/INPUT
-else
+#if [ $RESNAME = 500 ]; then
+  #export FIXDIR_PATH=/scratch2/NCEPDEV/climate/Denise.Worthen/soca/test/Data/72x35x25/INPUT
+#else
   #export FIXDIR_PATH=/scratch2/NCEPDEV/climate/climpara/S2S/FIX/fix_UFSp4/fix_mom6/${RESNAME}
   export FIXDIR_PATH=/scratch1/NCEPDEV/global/glopara/fix/mom6/20220805/${RESNAME}
-fi
 
 if [[ $MOSAICRES == C3072 ]]; then
     export NPX=3072
@@ -49,9 +50,12 @@ elif [[ $MOSAICRES == C192 ]]; then
 elif [[ $MOSAICRES == C96 ]]; then
   export MOSAICRES=C96
   export NPX=96
+elif [[ $MOSAICRES == C48 ]]; then
+  export MOSAICRES=C48
+  export NPX=48
 fi
 
-if [ $RESNAME = 400 ]; then
+if [ $RESNAME = 500 ]; then
   export NI=72
   export NJ=35
   export TOPOGFILE=ocean_topog.nc
@@ -64,12 +68,12 @@ if [ $RESNAME = 100 ]; then
   export MASKEDIT=.T.
   export TOPOGFILE=topog.nc
   export EDITSFILE=topo_edits_011818.nc
-  if [ $DO_POSTWGTS == .true. ]; then
+  #if [ $DO_POSTWGTS == .true. ]; then
    #pre-generate SCRIP files for dst rectilinear grids using NCO
    # TODO: is the stagger really correct? The first pt is at 0.0E?
    # should lat_type be cap? #lon_typ=grn_ctr#lat_typ=cap
-   ncremap -g ${OUTDIR_PATH}/rect.1p0_SCRIP.nc -G latlon=181,360#lon_typ=grn_ctr
-  fi
+      #$APRUN ncremap -g ${OUTDIR_PATH}/rect.1p0_SCRIP.nc -G latlon=181,360#lon_typ=grn_ctr#lat_typ=cap
+  #fi
 fi
 
 if [ $RESNAME = 050 ]; then
@@ -77,13 +81,13 @@ if [ $RESNAME = 050 ]; then
   export NJ=576
   export TOPOGFILE=ocean_topog.nc
   export EDITSFILE='none'
-  if [ $DO_POSTWGTS == .true. ]; then
+  #if [ $DO_POSTWGTS == .true. ]; then
    #pre-generate SCRIP files for dst rectilinear grids using NCO
    # TODO: is the stagger really correct? The first pt is at 0.0E?
    # should lat_type be cap? #lon_typ=grn_ctr#lat_typ=cap
-   ncremap -g ${OUTDIR_PATH}/rect.1p0_SCRIP.nc -G latlon=181,360#lon_typ=grn_ctr
-   ncremap -g ${OUTDIR_PATH}/rect.0p5_SCRIP.nc -G latlon=361,720#lon_typ=grn_ctr
-  fi
+      #export FSRC=${OUTDIR_PATH}/rect.1p00_SCRIP.nc
+      #export FSRC=${OUTDIR_PATH}/rect.0p50_SCRIP.nc
+  #fi
 fi
 
 if [ $RESNAME = 025 ]; then
@@ -91,14 +95,14 @@ if [ $RESNAME = 025 ]; then
   export NJ=1080
   export TOPOGFILE=ocean_topog.nc
   export EDITSFILE=All_edits.nc
-  if [ $DO_POSTWGTS == .true. ]; then
+  #if [ $DO_POSTWGTS == .true. ]; then
    #pre-generate SCRIP files for dst rectilinear grids using NCO
    # TODO: is the stagger really correct? The first pt is at 0.0E?
    # should lat_type be cap? #lon_typ=grn_ctr#lat_typ=cap
-   ncremap -g ${OUTDIR_PATH}/rect.1p0_SCRIP.nc -G latlon=181,360#lon_typ=grn_ctr
-   ncremap -g ${OUTDIR_PATH}/rect.0p5_SCRIP.nc -G latlon=361,720#lon_typ=grn_ctr
-   ncremap -g ${OUTDIR_PATH}/rect.0p25_SCRIP.nc -G latlon=721,1440#lon_typ=grn_ctr
-  fi
+      #$APRUN ncremap -g ${OUTDIR_PATH}/rect.1p0_SCRIP.nc -G latlon=181,360#lon_typ=grn_ctr#lat_typ=cap
+      #$APRUN ncremap -g ${OUTDIR_PATH}/rect.0p5_SCRIP.nc -G latlon=361,720#lon_typ=grn_ctr#lat_typ=cap
+      #$APRUN ncremap -g ${OUTDIR_PATH}/rect.0p25_SCRIP.nc -G latlon=721,1440#lon_typ=grn_ctr#lat_typ=cap
+  #fi
 fi
 
 if [ ! -d ${OUTDIR_PATH} ]; then
@@ -109,17 +113,18 @@ edit_namelist < grid.nml.IN > grid.nml
 make
 #srun -A nems --ntasks=2 ./gengrid
 #time srun -n 60 ./gengrid
-srun --nodes=4 --ntasks-per-node=5 ./gengrid
+#srun --nodes=4 --ntasks-per-node=5 ./gengrid
+srun -A nems --mem=12g --nodes=1 -t 00:30:00 ./gengrid
 
 # generate ice mesh
-export FSRC=${OUTDIR_PATH}/Ct.mx${RESNAME}_SCRIP_land.nc
-export FDST=${OUTDIR_PATH}/mesh.mx${RESNAME}.nc
-time srun -A nems -n 1 ESMF_Scrip2Unstruct ${FSRC} ${FDST} 0
+# export FSRC=${OUTDIR_PATH}/Ct.mx${RESNAME}_SCRIP_land.nc
+# export FDST=${OUTDIR_PATH}/mesh.mx${RESNAME}.nc
+# time srun -A nems -n 1 ESMF_Scrip2Unstruct ${FSRC} ${FDST} 0
 
 # generate kmt file for CICE
-export FSRC=${OUTDIR_PATH}/grid_cice_NEMS_mx${RESNAME}.nc
-export FDST=${OUTDIR_PATH}/kmtu_cice_NEMS_mx${RESNAME}.nc
-ncks -O -v kmt ${FSRC} ${FDST}
+# export FSRC=${OUTDIR_PATH}/grid_cice_NEMS_mx${RESNAME}.nc
+# export FDST=${OUTDIR_PATH}/kmtu_cice_NEMS_mx${RESNAME}.nc
+# ncks -O -v kmt ${FSRC} ${FDST}
 
 # clean up
 #make clean
