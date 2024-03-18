b7852a08f023fc6ef734481b01ac34cccab30aff
Denise Worthen <denise.worthen@noaa.gov>
2024-03-18 09:17:01 -0400

    update

--- gen_fixgrid.F90
+++ gen_fixgrid.F90
@@ -571,12 +571,12 @@ program gen_fixgrid
              ignoreDegenerate=.true., unmappedaction=ESMF_UNMAPPEDACTION_IGNORE, rc=rc)
         if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
              line=__LINE__, file=__FILE__)) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
         method=ESMF_REGRIDMETHOD_BILINEAR
         fdst = trim(dirout)//'/'//'Ct.mx'//trim(res)//'_SCRIP.nc'
         fwgt = trim(dirout)//'/'//'tripole.mx025.Ct.to.mx'//trim(res)//'.Ct.bilinear.nc'
         logmsg = 'creating weight file '//trim(fwgt)
         print '(a)',trim(logmsg)
-
         call ESMF_RegridWeightGen(srcFile=trim(fsrc),dstFile=trim(fdst), &
              weightFile=trim(fwgt), regridmethod=method,                 &
              ignoreDegenerate=.true., unmappedaction=ESMF_UNMAPPEDACTION_IGNORE, rc=rc)
@@ -589,7 +589,6 @@ program gen_fixgrid
      end if
   end if
 
-  ! tripole Bu->tripole Ct for CICE are generated in postweights
   ! tripole Ct->tripole Bu for CICE are only for CICE IC creation
   fsrc = trim(dirout)//'/'//'Ct.mx'//trim(res)//'_SCRIP.nc'
   fdst = trim(dirout)//'/'//'Bu.mx'//trim(res)//'_SCRIP.nc'
